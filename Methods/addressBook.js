1d983d0b5708c339e814ad8c9d51d133e7a0c088
Darshan <darshan.g.c.is@gmail.com>
2022-02-04 20:13:46 +0530

    fromserver2

--- Methods/addressBook.js
+++ Methods/addressBook.js
@@ -14,13 +14,13 @@ export const getForm = async (req, res) => {
     const data3 = await customerEntry.find();
     const data4 = await dispatcherEntry.find();
     const data5 = await ownerEntry.find();
-    const newData = {
-      brokerEntry: data1,
-      driverEntry: data2,
-      customerEntry: data3,
-      dispatcherEntry: data4,
-      ownerEntry: data5,
-    };
+    const newData = [
+      ...data1.map((item) => ({ ...item._doc, role: 'Broker' })),
+      ...data2.map((item) => ({ ...item._doc, role: 'Driver' })),
+      ...data3.map((item) => ({ ...item._doc, role: 'Customer' })),
+      ...data4.map((item) => ({ ...item._doc, role: 'Dispatcher' })),
+      ...data5.map((item) => ({ ...item._doc, role: 'Owner' })),
+    ];
     res.status(200).json(newData);
   } catch (error) {
     res.status(404).json({ message: error });
